43bdc9a689c3b2fd280f8ca2d1f85ca1e06532f4
Giulia <giulia.brugnatti@studio.unibo.it>
2021-07-21 23:42:07 +0200

    remove useless code

--- aws-project/public/js/sign-in.js
+++ aws-project/public/js/sign-in.js
@@ -1,34 +1,5 @@
 const SignIn = {
     template: `
-        <!--<div class="signInContainer">
-            <form>
-                <h3>Sign In</h3>
-    
-                <div class="form-group">
-                <label for="email">Email Address</label>
-                    <input type="text"
-                           id="email"
-                           class="form-control"
-                           v-model="userData.email">
-                </div>
-                 <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" 
-                           id="password"
-                           class="form-control"
-                           v-model="userData.password">
-                </div>
-                    <button class="btn btn-dark my-2 " @click="submitted">
-                        <router-link to="/rover-simulation">Sign-In</router-link>
-                    </button>  
-                    
-                    <p class=" text-right mt-2 mb-4">
-                        <router-link to="/forgot-password">Forgot password ?</router-link>
-                    </p>               
-             </form>
-
-        </div>
-        -->
             <div class="container">
         <div class="d-flex justify-content-center">
             <div class="mt-2 card col-12 col-sm-8 col-md-6 col-xl-5">
