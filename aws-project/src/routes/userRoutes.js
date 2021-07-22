6e61ef7f988a95367d0156eedefb3a22a717a199
Giulia <giulia.brugnatti@studio.unibo.it>
2021-07-22 09:30:54 +0200

    remove useless files

--- aws-project/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-    var usersController = require('../controllers/usersController');
-
-    app.route('/api/users')
-        .get(usersController.list_users())
-        .post(usersController.create_user());
-
-    app.use(usersController.show_index);
-};
