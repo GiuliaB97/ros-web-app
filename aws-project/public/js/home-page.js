fa7a3f05bf896b1b29bf44650476923a9bfc938e
Giulia <giulia.brugnatti@studio.unibo.it>
2021-07-31 16:06:04 +0200

    update nav bar ---> introduce routing issues

--- aws-project/public/js/home-page.js
+++ aws-project/public/js/home-page.js
@@ -1,7 +1,10 @@
 const HomePage = {
       template: `
-
+        
 		<div class="homeContainer">
+
+
+      
 			<div>
                     <h1>This is the home page</h1>
             </div>
@@ -23,5 +26,15 @@ const HomePage = {
 		</div>
 		
 	`,
+
+    method: {
+        openRegistrationForm: function() {
+            this.$router.replace('/registration').catch(err => {});
+            console.log("regist")
+        },
+        openLoginForm() {
+            this.$router.replace('/login').catch(err => {});
+        },
+    },
 
 }
--- aws-project/public/js/navigation-bar.js
+++ aws-project/public/js/navigation-bar.js
@@ -1,32 +1,28 @@
 const NavigationBar =  {
-    components: {
-        //'signUpButton': SignUpButton,
-        //'signInButton': SignInButton,
-    },
-    data: function () {
-    },
     template: `
-  <!-- Image and text -->
-<!-- BARRA DI NAVIGAZIONE-->
    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
         <!-- BRAND -->
-        <a class="navbar-brand" href="#">RosWebApp</a>
+        <a class="navbar-brand">RosWebApp</a>
 
-        <!-- RESPONSIVE MENU-->      
-        <!-- MENU -->
         <div class="nav-item ">
-            <button class="btn btn-secondary my-2 my-sm-0" type="submit" to="\registration">
-                <router-link to="/registration">Sign-Up</router-link>
-            </button>
-            
-            <button class="btn btn-dark my-2 my-sm-0" type="submit" to="\rover-simulation">
-                <router-link to="/login">Sign-In</router-link>
-            </button>
-        <!-- FINE RESPONSIVE MENU --> 
-        </div>
+         <button @click="this.openLoginForm">Sign In</button>
+         <button  @click="this.openRegistrationForm" class="btn-dark my-2 my-sm-0" >SIgn up</button>
+        <!--
+        <input type="button" @click="openRegistrationForm" value="Login" v-class="btn btn-success rounded-pill"/>Sign-Up</input>
         
-        <!-- FINE NAVBAR --> 
+            <button class="btn btn-dark my-2 my-sm-0" type="submit" @click="openLoginForm">Sign-In</button>
+        -->
+        </div>
       </nav>
     `,
 
+    method: {
+        openRegistrationForm: function() {
+            this.$router.replace('/registration').catch(err => {});
+            console.log("regist")
+        },
+        openLoginForm() {
+            this.$router.replace('/login').catch(err => {});
+        },
+    },
 }
