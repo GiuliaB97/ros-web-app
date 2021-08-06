7a4c63e107ff6649f9301a718435d2c1c80d1577
Giulia <giulia.brugnatti@studio.unibo.it>
2021-08-06 12:43:30 +0200

    add link for footer icons

--- aws-project/public/js/home-footer.js
+++ aws-project/public/js/home-footer.js
@@ -2,14 +2,28 @@ const HomeFooter = {
     template: `
         <div class="footer-basic" >
             <footer>
-                <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+                <div class="social">
+                    <a href="https://www.instagram.com/almax.rover/?hl=it">
+                        <i class="icon ion-social-instagram"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/company/alma-x/?originalSubdomain=it">
+                        <i class="icon ion-social-linkedin"></i>
+                    </a>
+                    <a href="https://twitter.com/uniboroverteam">
+                        <i class="icon ion-social-twitter"></i>
+                    </a>
+                    <a href="https://it-it.facebook.com/Uniboroverteam/"><i class="icon ion-social-facebook"></i>
+                    </a>
+                </div>
+                <!--
                 <ul class="list-inline">
-                    <li class="list-inline-item"><a href="#">Home</a></li>
+                    <li class="list-inline-item"><a href="\">Home</a></li>
                     <li class="list-inline-item"><a href="#">Services</a></li>
                     <li class="list-inline-item"><a href="#">About</a></li>
                     <li class="list-inline-item"><a href="#">Terms</a></li>
                     <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
                 </ul>
+                -->
                 <p class="copyright">Almax © 2018</p>
             </footer>
         </div>
