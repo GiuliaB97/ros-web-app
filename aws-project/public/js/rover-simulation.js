75da7b9930f8a63d05fda8b1afb35dff6620bfc6
Giulia <giulia.brugnatti@studio.unibo.it>
2021-08-08 12:13:10 +0200

    refactor: create rover-setting file for offcanvas component

--- aws-project/public/js/rover-simulation.js
+++ aws-project/public/js/rover-simulation.js
@@ -21,34 +21,10 @@ const RoverSimulation = {
               <button type="button" @click="disconnect" class="btn btn-danger rounded-pill btn-lg" v-if="connected"  data-toggle="tooltip" data-placement="top" title="Click here to tear down the connection ">Disconnect!</button>
               <button  type="button"  @click="connect" class="btn btn-success rounded-pill btn-lg" v-else  data-toggle="tooltip" data-placement="top" title="Click here to connect to the simulation">Connect!</button>
               <button class="btn btn-secondary btn-lg position-absolute end-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Settings</button>
-              
+
             </div>
-            <!--offcanvas-->
-            <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Workspace advanced configuration</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                <div class=" mt-3">
-                  <label class="row col-form-label">Connection status:</label>
-                    <input type="text" class="form-control" id="connectionStatus" v-if="connected" value="connected" readonly>
-                    <input type="text" class="form-control" id="connectionStatus" v-else value="Not connected" readonly>
-                  
-                </div>
-                <div class=" mt-3">
-                  <label class="col-form-label">Websocket server address is:</label>
-                    <input type="text" class="form-control" id="ws_address" v-model="ws_address" readonly>
-                </div>
-                
-                <div class="mt-3">
-                  <!-- Button trigger modal -->
-                  <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Advanced options</button>
-                  
-                </div>
-                   
-                  </div>
-                </div>
+            <rover-setting :connected="connected" :ws_address="ws_address"></rover-setting>
+
                 
                 <!-- Modal -->
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
@@ -93,6 +69,7 @@ const RoverSimulation = {
         'roverCommands': RoverCommands,
         'roverVideo': RoverVideo,
         'roverCharts': RoverCharts,
+        'roverSetting': RoverSetting,
     },
     data () {
         return {
