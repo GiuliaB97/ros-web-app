f451292ab464676c4ced6442672da2b4e861e598
Giulia <giulia.brugnatti@studio.unibo.it>
2021-08-13 13:42:23 +0200

    remove useless tag for position alignment

--- aws-project/public/js/rover-commands.js
+++ aws-project/public/js/rover-commands.js
@@ -1,9 +1,9 @@
 const RoverCommands = {
     props: ['connected'],
     template: `
-      <div id="rover controller"v-if="connected" class="row" >
+      <div id="rover controller"v-if="connected" class="row">
       <!--<div id="rover controller" class="row">-->
-       <div class="col-md-12 text-center">
+       <div class=" text-center">
          <h5>Commands</h5>
          
        </div>
