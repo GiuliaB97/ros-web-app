b948c5c3b8e4312ab5da70b685ea0248de4e75f2
Giulia <giulia.brugnatti@studio.unibo.it>
2021-08-17 20:46:31 +0200

    remove unused code

--- aws-project/src/models/userModel.js
+++ aws-project/src/models/userModel.js
@@ -1,12 +1,12 @@
 module.exports = function(mongoose) {
     const Schema = mongoose.Schema;
     const UserSchema = new Schema({
-        name: String, // String is shorthand for {type: String}
-        surname: String, // String is shorthand for {type: String}
-        email: String, // String is shorthand for {type: String}
+        name: String,
+        surname: String,
+        email: String,
         user_id: String,
         password: String,
-        salt: String, // String is shorthand for {type: String}
+        salt: String,
     }, {
         versionKey: false
     });
--- aws-project/src/routes/usersRoutes.js
+++ aws-project/src/routes/usersRoutes.js
@@ -1,4 +1,4 @@
-const usersController = require("../controllers/usersController");
+
 module.exports = function(app) {
 	var usersController = require('../controllers/usersController');
 
