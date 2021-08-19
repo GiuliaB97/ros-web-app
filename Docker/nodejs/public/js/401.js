dc80b20c75c11d6faaa608b23c80223ee9e3d3fa
Giulia <giulia.brugnatti@studio.unibo.it>
2021-08-19 17:22:55 +0200

    remove duplicated code

--- Docker/nodejs/public/js/401.js
+++
@@ -1,3 +0,0 @@
-const NotAuthorized = {
-    template:
-        '<div><img src="static/img/401.jpg" id="notauthorized"/> </div>' }
--- Docker/nodejs/public/js/404.js
+++
@@ -1,8 +0,0 @@
-const NotFound = {
-    template: '<div><img src="static/img/404.png" id="notFound" @click="openHome"/></div>' ,
-    methods: {
-        openHome(){
-            this.$router.replace('/').catch(err => {})
-        },
-    }
-}
