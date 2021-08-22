034378afb9443575e85a7f917e26a513b493659f
Giulia <giulia.brugnatti@studio.unibo.it>
2021-08-22 20:01:27 +0200

    update docker directory removing useless file + update index to fix mongoose connection using docker

--- Docker/docker-compose/up.sh
+++
@@ -1,3 +0,0 @@
- docker network create -d bridge local_network
- docker run  -itd  --network local_network  -p 27017-27019:27017-27019  --name mongodb mymongo
- docker run -itd --rm --network local_network --name nodejsapp -p 7000:7000 nodejsapp
